777b8bd22d0d7215440f8945f60749e8307387a1
Paul Smith <code@uvwxy.de>
2012-09-26 15:56:11 +0200

    moved GameClient / GameServer to library, preparing code for game state exchange

--- src/de/uvwxy/reversi/ActivityGame.java
+++ src/de/uvwxy/reversi/ActivityGame.java
@@ -2,21 +2,33 @@ package de.uvwxy.reversi;
 
 import java.io.IOException;
 
-import de.uvwxy.packsock.chat.ChatMessage;
-import de.uvwxy.packsock.chat.IChatMessageHook;
+import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.app.Activity;
-import android.content.DialogInterface;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
+import de.uvwxy.packsock.chat.ChatMessage;
+import de.uvwxy.packsock.chat.IChatMessageHook;
+import de.uvwxy.packsock.game.GameClient;
+import de.uvwxy.packsock.game.GameMessage;
+import de.uvwxy.packsock.game.GameServer;
+import de.uvwxy.packsock.game.IGameMessageHook;
 
 public class ActivityGame extends Activity {
+
+	// SETUP UI
+	private EditText etServer = null;
+	private EditText etPort = null;
+	private EditText etName = null;
+	private CheckBox cbSpectators = null;
+
+	// GAME UI
 	private Button btnSend = null;
 	private EditText etChat = null;
 	private EditText etInput = null;
@@ -24,14 +36,34 @@ public class ActivityGame extends Activity {
 	GameServer server;
 
 	private UpdateUITask uiUpdater = new UpdateUITask();
-	
-	private IChatMessageHook mh0 = new IChatMessageHook() {
+
+	private IChatMessageHook clientChatMessageReceived = new IChatMessageHook() {
 
 		@Override
 		public void onMessageReceived(ChatMessage msg) {
 			if (etChat != null) {
 				uiUpdater.execute(msg);
 			}
+
+		}
+
+	};
+
+	private IGameMessageHook clientGameMessageReceived = new IGameMessageHook() {
+
+		@Override
+		public void onMessageReceived(GameMessage msg) {
+			// TODO Auto-generated method stub
+
+		}
+
+	};
+
+	private IGameMessageHook serverGameMessageReceived = new IGameMessageHook() {
+
+		@Override
+		public void onMessageReceived(GameMessage msg) {
+			// TODO Auto-generated method stub
 
 		}
 
@@ -77,31 +109,156 @@ public class ActivityGame extends Activity {
 
 	};
 
-	private class NetworkThread implements Runnable {
+	private class ServerThread implements Runnable {
+
+		private int port;
+
+		public ServerThread(int port) {
+			this.port = port;
+		}
 
 		@Override
 		public void run() {
-			server = new GameServer(25567, 0, "Reversi Server");
+			// TODO: check param "0" for spectator function?
+			server = new GameServer(port, 0, "Server", serverGameMessageReceived);
 			try {
 				server.start();
 			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-			client = new GameClient(25567, "localhost", mh0);
-
-			try {
-				client.connect();
-			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 
 	}
 
+	private class ClientThread implements Runnable {
+
+		private int port;
+		private String address;
+
+		public ClientThread(int port, String address) {
+			this.port = port;
+			this.address = address;
+		}
+
+		@Override
+		public void run() {
+			// TODO: check param "0" for spectator function?
+			client = new GameClient(port, address, clientChatMessageReceived, clientGameMessageReceived);
+			try {
+				client.connect();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		createSetupUI();
+
+	}
+
+	public void hostGame(View v) {
+		boolean all_ok = true;
+
+		String name = etName.getText().toString();
+		if (name == null || name.equals("")) {
+			if (etName.getText().toString().equals("")) {
+				etName.setText("User_" + (char) (Math.random() * 128));
+				all_ok = false;
+			}
+			name = etName.getText().toString();
+		}
+
+		int port = 0;
+		try {
+			port = Integer.parseInt(etPort.getText().toString());
+		} catch (Exception e) {
+			port = 0;
+			all_ok = false;
+		}
+		if (port <= 0 || port >= 65535) {
+			port = (int) (Math.random() * 256) + 25500;
+			all_ok = false;
+		}
+		etPort.setText("" + port);
+
+		Thread t = new Thread(new ServerThread(port));
+		t.start();
+
+		if (all_ok) {
+			// TODO: connect as client aswell and then create game ui
+			createGameUI();
+		} else {
+			longToast("Your input was not 100% correct. Guessing further settings. Click again to start with new values");
+		}
+	}
+
+	public void connectGame(View v) {
+		boolean all_ok = true;
+
+		String name = etName.getText().toString();
+		if (name == null || name.equals("")) {
+			if (etName.getText().toString().equals("")) {
+				etName.setText("User_" + (char) (Math.random() * 128));
+				all_ok = false;
+			}
+			name = etName.getText().toString();
+		}
+
+		int port = 0;
+		try {
+			port = Integer.parseInt(etPort.getText().toString());
+		} catch (Exception e) {
+			port = 0;
+			all_ok = false;
+		}
+		if (port <= 0 || port >= 65535) {
+			port = (int) (Math.random() * 256) + 25500;
+			all_ok = false;
+		}
+		etPort.setText("" + port);
+
+		String server = etServer.getText().toString();
+
+		if (server.equals("")) {
+			all_ok = false;
+			longToast("Please enter a valid server address");
+		}
+
+		if (!all_ok) {
+			longToast("Your input was not 100% correct. Guessing further settings. Click again to start with new values");
+			return;
+		}
+
+		clientConnect(server, port, name);
+
+	}
+
+	public void clientConnect(String server, int port, String name) {
+		client = new GameClient(port, server, clientChatMessageReceived, clientGameMessageReceived);
+
+		try {
+			client.connect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void createSetupUI() {
+		setContentView(R.layout.activity_setup);
+
+		etServer = (EditText) findViewById(R.id.etSever);
+		etPort = (EditText) findViewById(R.id.etPort);
+		etName = (EditText) findViewById(R.id.etName);
+		cbSpectators = (CheckBox) findViewById(R.id.cbSpectators);
+
+	}
+
+	private void createGameUI() {
 		setContentView(R.layout.activity_reversi);
 
 		btnSend = (Button) findViewById(R.id.btnSend);
@@ -109,13 +266,13 @@ public class ActivityGame extends Activity {
 		etChat = (EditText) findViewById(R.id.etChat);
 		etInput = (EditText) findViewById(R.id.etInput);
 
-		Thread t = new Thread(new NetworkThread());
-		t.start();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_reversi, menu);
-		return true;
+	private void longToast(String msg) {
+		Context context = getApplicationContext();
+		int duration = Toast.LENGTH_LONG;
+
+		Toast toast = Toast.makeText(context, msg, duration);
+		toast.show();
 	}
 }
