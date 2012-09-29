dde65f6952aba30dd0cad0508c8dedda5c233841
U-MrBook\Paul <Paul@MrBook.(none)>
2012-09-29 16:37:06 +0200

    manage imports

--- src/de/uvwxy/reversi/ActivityGame.java
+++ src/de/uvwxy/reversi/ActivityGame.java
@@ -10,8 +10,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
